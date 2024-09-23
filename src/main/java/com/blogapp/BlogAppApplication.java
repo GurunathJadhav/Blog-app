c2e48d5dd07fc3f8d09b37667c98676cb83541e8
Gurunath Jadhav <gurunath.jadhav.842@gmail.com>
2024-09-23 23:11:44 +0530

    make our project to test on tomcat server

--- src/main/java/com/blogapp/BlogAppApplication.java
+++ src/main/java/com/blogapp/BlogAppApplication.java
@@ -1,18 +1,22 @@
 package com.blogapp;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogAppApplication {
+public class BlogAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApplication.class, args);
 
 	}
 
-
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BlogAppApplication.class);
+	}
 }
